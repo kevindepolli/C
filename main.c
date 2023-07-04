95660711b61985cd3b3075b690b66fd15ecb1be9
GuilhermeAntonyDutraMerlo <Guilherme_antony.dutra@hotmail.com>
2023-07-04 10:48:12 -0300

    tabelaHash_v1

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tabelaHash.h"
+
+int main(){
+    struct aluno al, al2;
+    printf("informe a matricula: ");
+    scanf("%d", &al.matricula);
+    printf("Informe o nome: ");
+    scanf("%s", &al.nome);
+    printf("Informe a nota 1: ");
+    scanf("%f", &al.n1);
+
+    Hash *ha;
+    ha = criaHash(1427);
+
+    int x = insereHash_SemColisao(ha, al);
+
+    int busca;
+    printf("Matricula para busca: ");
+    scanf("%d", &busca);
+    int resultado = buscaHash_SemColisao(ha, busca, &al2);
+
+    if(resultado != 0){
+        printf("Encontrou o aluno %s\n", al2.nome);
+    }
+
+
+
+    int x = insereHash_EnderAberto(ha, al);
+
+    int x = buscaHash_EnderAberto(ha, mat, &al);
+    
+    liberaHash(ha);
+    return 0;
+}
